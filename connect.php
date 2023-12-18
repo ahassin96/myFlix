33944a0b1734597bc0cfe049e73d42e93f06895b
Alistair Hassin <alistairh96@outlook.com>
2023-12-18 15:43:17 +0000

    connection page 7 further changes

--- connect.php
+++ connect.php
@@ -1,22 +1,26 @@
 <?php
-class DbConnect{
+class DbConnect {
+    public string $host;
+    public string $user;
+    public string $password;
+    public string $dbname;
 
-  public function __construct() {
-        $this->host = "ec2-user@ec2-52-20-195-51.compute-1.amazonaws.com";
+    public function __construct() {
+        $this->host = "ec2-52-20-195-51.compute-1.amazonaws.com";
         $this->user = getenv('DB_USERNAME');
         $this->password = getenv('DB_PASSWORD');
         $this->dbname = "myflixdb";
     }
- 
 
- public function connect(){
-    try {
-        $conn = new PDO('mysql:host=' . $this->host . '; dbname=' . $this->dbname, $this->user, $this->password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $conn;
-    } catch( PDOException $e) {
-        echo 'Database Error: ' . $e->getMessage();
+    public function connect() {
+        try {
+            $conn = new PDO('mysql:host=' . $this->host . '; dbname=' . $this->dbname, $this->user, $this->password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        } catch(PDOException $e) {
+            echo 'Database Error: ' . $e->getMessage();
+            return null; // Handle the error appropriately in your application
+        }
     }
-}
 }
 ?>
