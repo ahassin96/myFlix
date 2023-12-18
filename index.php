7d3ec762fd330839e7ca02328a7461a4f444efa5
Alistair Hassin <alistairh96@outlook.com>
2023-12-18 15:04:17 +0000

    connection page 3

--- index.php
+++ index.php
@@ -16,6 +16,37 @@ $conn = $conn->connect();
 		<p> this is the body</p>
 		<p> this is the new text added for apache test</p>		
 		<p> second test </p>
+		<?php 
+
+		$sql = "SELECT * FROM UserAccounts";
+			$stmt = $pdo->prepare($sql);
+
+			// Execute the query
+			try {
+			    $stmt->execute();
+			    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+			    // Display the results
+			    if (!empty($result)) {
+			        echo "<ul>";
+			        foreach ($result as $row) {
+			            echo "<li>";
+			            foreach ($row as $key => $value) {
+			                echo "$key: $value, ";
+			            }
+			            echo "</li>";
+			        }
+			        echo "</ul>";
+			    } else {
+			        echo "No records found";
+			    }
+			} catch (PDOException $e) {
+			    die("Query failed: " . $e->getMessage());
+			}
+			?>
+
+
+		 ?>
 
 </body>
 </html>
