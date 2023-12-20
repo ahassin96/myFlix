881a7ceea993081ed1f7516afa53a8270d161dc6
Alistair Hassin <alistairh96@outlook.com>
2023-12-20 21:21:28 +0000

    add create user account 4

--- index.php
+++ index.php
@@ -1,37 +1,45 @@
-<?php  session_start();
+<?php
+session_start();
 require_once "connect.php";
 $conn = new DbConnect();
-$conn = $conn->connect();
-if(!isset($_SESSION["username"])){
-    header("Location:login.php");
-} 
+$pdo = $conn->connect();
+
+if (!isset($_SESSION["username"])) {
+    header("Location: login.php");
+}
 $userId = $_SESSION["user_id"];
 
-           
-
-                $stmt = $pdo->prepare("SELECT * FROM UserProfiles WHERE user_id = :user_id");
-                $stmt->bindParam(':user_id', $userId, PDO::PARAM_INT);
-                $stmt->execute();
-                $profiles = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-
+$stmt = $pdo->prepare("SELECT * FROM UserProfiles WHERE user_id = :user_id");
+$stmt->bindParam(':user_id', $userId, PDO::PARAM_INT);
+$stmt->execute();
+$profiles = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
-
 <!DOCTYPE html>
-
 <html>
+
 <head>
-	<title> hello this is new change </title>
+    <title>Welcome to Your Profiles</title>
 </head>
+
 <body>
-		<h1> Welcome <?php echo $_SESSION["username"];?></h1>
-            <a href = "logout.php">Exit</a>
-            <?php if (empty($profiles)): ?>
-            <p>No profiles found for this user.</p>
-            <?php else: ?>
+    <h1>Welcome <?php echo $_SESSION["username"]; ?></h1>
+    <a href="logout.php">Logout</a>
 
-
-		
+    <?php if (empty($profiles)): ?>
+        <p>No profiles found for this user.</p>
+    <?php else: ?>
+        <h2>Your Profiles:</h2>
+        <ul>
+            <?php foreach ($profiles as $profile): ?>
+                <li>
+                    <strong>Profile Name:</strong> <?php echo $profile['profile_name']; ?><br>
+                    
+                    <a href="profile.php?profile_id=<?php echo $profile['profile_id']; ?>">View Profile</a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
 </body>
+
 </html>
