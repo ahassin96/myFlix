c1a7bd357fda75f0a82bb0dc7aa14783732ce0b8
Alistair Hassin <alistairh96@outlook.com>
2023-12-21 13:02:46 +0000

    mongodb implementation

--- profile.php
+++ profile.php
@@ -1,3 +1,25 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$mongoClient = new MongoDB\Client("mongodb://localhost:27017");
+
+
+$database = $mongoClient->myflix;
+
+
+$collection = $database->children;
+
+
+
+$cursor = $collection->find();
+
+
+foreach ($cursor as $document) {
+    var_dump($document);
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
