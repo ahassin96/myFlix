06cd4c52e7aae826cc9b41933a45f27e0b989626
Alistair Hassin <alistairh96@outlook.com>
2023-12-22 10:59:12 +0000

    add videos to website 3

--- profile.php
+++ profile.php
@@ -10,7 +10,6 @@ $database = $mongoClient->myflix;
 
 $genres = ['horror', 'military', 'action'];
 
-
 ?>
 
 <!DOCTYPE html>
@@ -18,41 +17,42 @@ $genres = ['horror', 'military', 'action'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>MyFlix Video Library</title>
 </head>
 <body>
     <h1>MyFlix Video Library</h1>
 
-    <?php foreach ($genres as $genre) {
-       
-        $videos = $database->videos->find(['genre' => $genre]);
-
-       
-        ?>
-        <div class="video-container" id="<?php echo $genre; ?>-container">
-            <h2><?php echo ucfirst($genre); ?></h2>
-        <?php
-
-     
-        foreach ($videos as $video) {
-            
+    <?php
+    try {
+        foreach ($genres as $genre) {
+            $videos = $database->videos->find(['genre' => $genre]);
             ?>
-            <div class="video">
-                <video controls>
-                    <source src="<?php echo $video['url']; ?>" type="video/mp4">
-                    Your browser does not support the video tag.
-                </video>
-                <p><?php echo $video['title']; ?></p>
+
+            <div class="video-container" id="<?php echo $genre; ?>-container">
+                <h2><?php echo ucfirst($genre); ?></h2>
+
+                <?php
+                foreach ($videos as $video) {
+                    ?>
+                    <div class="video">
+                        <video controls>
+                            <source src="<?php echo $video['url']; ?>" type="video/mp4">
+                            Your browser does not support the video tag.
+                        </video>
+                        <p><?php echo $video['title']; ?></p>
+                    </div>
+                    <?php
+                }
+                ?>
             </div>
+
             <?php
         }
-
-        ?>
-        </div>
-        <?php
+    } catch (Exception $e) {
+        echo "Error: " . $e->getMessage();
     }
-
     ?>
-    </body>
-    </html>
+
+</body>
+</html>
