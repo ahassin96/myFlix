55359908fdfc1d383bfe7b2cd6a68ef03705dca1
Alistair Hassin <alistairh96@outlook.com>
2024-01-11 17:34:18 +0000

    testing have one page for scripts

--- watch.php
+++ watch.php
@@ -2,7 +2,30 @@
 session_start();
 require 'vendor/autoload.php';
 
+use GraphAware\Neo4j\Client\ClientBuilder;
 use MongoDB\Client;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$neo4jConnection = [
+    'host' => $_ENV['NEO4J_HOST'],
+    'port' => $_ENV['NEO4J_PORT'],
+    'username' => $_ENV['NEO4J_USERNAME'],
+    'password' => $_ENV['NEO4J_PASSWORD'],
+];
+
+$connectionUrl = sprintf(
+    'bolt://%s:%s@%s:%s',
+    $neo4jConnection['username'],
+    $neo4jConnection['password'],
+    $neo4jConnection['host'],
+    $neo4jConnection['port']
+);
+
+$clientNeo4j = ClientBuilder::create()
+    ->addConnection('default', $connectionUrl)
+    ->build();
 
 $mongoClient = new Client("mongodb://ec2-54-221-90-30.compute-1.amazonaws.com:27017");
 $database = $mongoClient->admin;
@@ -12,6 +35,22 @@ $userAccount = $_SESSION['user_id'];
 $Profile = $_SESSION['userProfile'];
 
 try {
+   
+    $clientNeo4j->run("MERGE (u:User {_id: {userId}})", ['userId' => $userAccount]);
+    $clientNeo4j->run("MERGE (v:Video {_id: {videoId}})", ['videoId' => $videoId]);
+
+    $clientNeo4j->run(
+        "MATCH (u:User {_id: {userId}})
+        MATCH (v:Video {_id: {videoId}})
+        MERGE (u)-[:WATCHED {profile: {userProfile}}]->(v)",
+        [
+            'userId' => $userAccount,
+            'videoId' => $videoId,
+            'userProfile' => $Profile,
+        ]
+    );
+
+   
     $videoDetails = $database->movies->findOne(['_id' => new MongoDB\BSON\ObjectId($videoId)]);
 
     if ($videoDetails) {
@@ -19,22 +58,10 @@ try {
         <!DOCTYPE html>
         <html lang="en">
         <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-            <link rel="stylesheet" href="css/style.css">
-            <title>Watch Video - MyFlix</title>
+            
         </head>
         <body>
-            <h1>Watch Video - MyFlix</h1>
-            <h2><?php echo $videoDetails['title']; ?></h2>
-            <p><?php echo $videoDetails['description']; ?></p>
-
-            <video id="watchVideo" controls>
-                <source src="<?php echo $videoDetails['url']; ?>" type="video/mp4">
-                Your browser does not support the video tag.
-            </video>
-            <p>test</p>
+    
             <script>
                 $(document).ready(function() {
                     var videoId = "<?php echo $videoId; ?>";
