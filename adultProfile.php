503c359d12349d570e67037c48f8a0476ad22fb3
Alistair Hassin <alistairh96@outlook.com>
2024-01-12 20:33:29 +0000

    another test 17

--- adultProfile.php
+++ adultProfile.php
@@ -27,6 +27,7 @@ if (isset($_GET['selectedProfile'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <title>MyFlix Video Library</title>
 </head>
 <body>
