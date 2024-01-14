6cc4b60d36d52386b78dd2c5fdd6046ff124a056
Alistair Hassin <alistairh96@outlook.com>
2024-01-14 21:44:17 +0000

    commit 4

--- watch.php
+++ watch.php
@@ -23,6 +23,7 @@ if ($response !== false) {
         $videoTitle = isset($videoDetails['video_details']['title']) ? $videoDetails['video_details']['title'] : 'Video Title Not Available';
         $videoDescription = isset($videoDetails['video_details']['description']) ? $videoDetails['video_details']['description'] : 'Video Description Not Available';
         $videoUrl = isset($videoDetails['video_details']['url']) ? $videoDetails['video_details']['url'] : '';
+        $videoTags = isset($videoDetails['video_details']['tags']) ? $videoDetails['video_details']['tags'] : [];
 
     } else {
         echo "Error: Video details not found.";
@@ -65,6 +66,7 @@ if ($response !== false) {
                 var videoId = "<?php echo $videoId; ?>";
                 var userId = "<?php echo $_SESSION['user_id']; ?>";
                 var userProfile = "<?php echo $_SESSION['userProfile']; ?>";
+                var videoTags = <?php echo json_encode($videoTags); ?>;
 
                 $.ajax({
                     type: "POST",
@@ -73,7 +75,9 @@ if ($response !== false) {
                     data: JSON.stringify({
                         user_id: userId,
                         user_profile: userProfile,
-                        video_id: videoId
+                        video_id: videoId,
+                        tags: videoTags
+                        
                     }),
                     success: function (data) {
                         console.log("Watched video recorded successfully:", data);
