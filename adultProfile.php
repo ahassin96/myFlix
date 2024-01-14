7ad55048c78012f5ad8efa823b46c85349796014
Alistair Hassin <alistairh96@outlook.com>
2024-01-14 15:15:06 +0000

    commit 23

--- adultProfile.php
+++ adultProfile.php
@@ -50,7 +50,8 @@ if (isset($_GET['selectedProfile'])) {
                     ?>
                     <div class="video">
                         <p><?php echo $video['title']; ?></p>
-                        <a href="watch.php">Watch Details</a>
+                        <a href="watch.php?id=<?php echo $video['id']; ?>">Watch Details</a>
+
                         <video controls>
                             <source src="<?php echo $video['url']; ?>" type="video/mp4">
                             Your browser does not support the video tag.
