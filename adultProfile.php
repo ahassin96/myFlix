8d1a93c0158fe5cc7f70088041c14dcfcbb5d8ee
Alistair Hassin <alistairh96@outlook.com>
2024-01-14 12:43:55 +0000

    commit 9

--- adultProfile.php
+++ adultProfile.php
@@ -48,7 +48,8 @@ if (isset($_GET['selectedProfile'])) {
                     ?>
                     <div class="video">
                         <p><?php echo $video['title']; ?></p>
-                           <a href="http://3.90.74.38:5000/watch/<?php echo $video['_id']; ?>">Watch Details</a>
+                           <a href="watch.php?id=<?php echo $video['_id']; ?>">Watch Details</a>
+
 
 
                         <video controls>
--- watch.php
+++ watch.php
@@ -5,6 +5,10 @@ require 'vendor/autoload.php';
 $videoId = isset($_GET['id']) ? $_GET['id'] : null;
 $userAccount = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null;
 $Profile = isset($_SESSION['userProfile']) ? $_SESSION['userProfile'] : null;
+
+$apiUrl = 'http://3.90.74.38:5000/watch/' . $videoId;
+$videoDetailsJson = file_get_contents($apiUrl);
+$videoDetails = json_decode($videoDetailsJson, true);
 ?>
 
 <!DOCTYPE html>
@@ -21,26 +25,14 @@ $Profile = isset($_SESSION['userProfile']) ? $_SESSION['userProfile'] : null;
     <h1>Watch Video - MyFlix</h1>
 
     <div id="videoDetailsContainer">
-        <h2 id="videoTitle">Video Title Not Available</h2>
-        <p id="videoDescription">Video Description Not Available</p>
+        <h2 id="videoTitle"><?php echo isset($videoDetails['title']) ? $videoDetails['title'] : 'Video Title Not Available'; ?></h2>
+        <p id="videoDescription"><?php echo isset($videoDetails['description']) ? $videoDetails['description'] : 'Video Description Not Available'; ?></p>
     </div>
 
     <video id="watchVideo" controls>
+        <source src="<?php echo isset($videoDetails['url']) ? $videoDetails['url'] : ''; ?>" type="video/mp4">
         Your browser does not support the video tag.
     </video>
 
-    <script>
-    $(document).ready(function() {
-        var videoId = "<?php echo $videoId; ?>";
-        var userAccount = "<?php echo $userAccount; ?>";
-        var userProfile = "<?php echo $Profile; ?>";
-
-        $.ajax({
-            type: 'GET',
-            url: 'http://3.90.74.38:5000/watch/' + videoId, 
-            success: function(response) {
-                console.log('Video details:', response.video_details);
-
-                if (response.video_details) {
-                    $('#videoTitle').text(response.video_details.title);
-                    $('#videoDescription').text(response
+</body>
+</html>
