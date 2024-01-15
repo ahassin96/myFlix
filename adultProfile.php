73c4271a5fccefcf17335fdc33e7e63e3f171571
Alistair Hassin <alistairh96@outlook.com>
2024-01-15 16:13:29 +0000

    new commit 4

--- adultProfile.php
+++ adultProfile.php
@@ -35,30 +35,35 @@ if (isset($_GET['selectedProfile'])) {
 
 
     <?php
-     try {
+    try {
         $flask_url = "http://3.90.74.38:9092/recommendations/" . $_SESSION['user_id'];
         $json_data = file_get_contents($flask_url);
         $recommendations = json_decode($json_data, true)['recommendations'];
-        ?>
 
-        <div class="video-container" id="recommendations-container">
-            <h2>Recommendations</h2>
-            <?php
-            foreach ($recommendations as $recommendation) {
-                ?>
-                <div class="video">
-                    <p>Recommendation: <?php echo $recommendation['video_id']; ?></p>
-                    <p>Tags: <?php echo implode(', ', $recommendation['tags']); ?></p>
-                </div>
-                <?php
-            }
+        if (!empty($recommendations)) {
+            $firstRecommendation = reset($recommendations);
+            $video_id = $firstRecommendation['video_id'];
+            $tags = implode(', ', $firstRecommendation['tags']);
             ?>
-        </div>
-        <?php
-        
+
+            <div class="video-container" id="recommendations-container">
+                <h2>Recommendation</h2>
+                <div class="video">
+                    <p>Recommendation: <?php echo $video_id; ?></p>
+                    <p>Tags: <?php echo $tags; ?></p>
+
+                    <video controls>
+                        <source src="http://3.90.74.38:9090/movies/<?php echo $video_id; ?>" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
+            </div>
+            <?php
+        }
     } catch (Exception $e) {
         echo "Error: " . $e->getMessage();
     }
+    ?>
 
     try {
         foreach ($genres as $genre) {
